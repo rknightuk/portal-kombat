10c55e5856524c7a76a370498a5c12acc808a41c
Robb Knight <desk@rknight.me>
2025-03-24 15:53:47 +0000

    remove

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>hpsc25</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <title>boats?</title>
 
         <!--
 ____
