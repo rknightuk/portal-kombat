6e68cd9609f5c040591f74e0f4e0b5c62e88f3e4
Robb Knight <desk@rknight.me>
2025-03-26 12:10:37 +0000

    icons and fonts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,16 @@
 
         <link href="https://uchu.style/color.css" rel="stylesheet"/>
 
+        <link rel="icon" type="image/png" href="/assets/icons/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/assets/icons/favicon.svg" />
+        <link rel="shortcut icon" href="/assets/icons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/assets/icons/site.webmanifest" />
+
         <style>
             body {
+                padding: 2em;
                 background-color: var(--uchu-light-yellow);
                 color: var(--uchu-dark-blue);
                 display: flex;
@@ -17,6 +25,51 @@
                 justify-content: center;
                 flex-direction: column;
                 font-family: 'Nimbus Mono PS', 'Courier New', monospace;
+                font-weight: normal;
+            }
+
+            @font-face {
+                font-family: 'ShipsWhistle-Bold';
+                src: url('/assets/fonts/ShipsWhistle-Bold.woff2 ') format('woff2');
+                font-display: swap;
+            }
+            @font-face {
+                font-family: 'ShipsWhistle-BoldItalic';
+                src: url('/assets/fonts/ShipsWhistle-BoldItalic.woff2 ') format('woff2');
+                font-display: swap;
+            }
+            @font-face {
+                font-family: 'ShipsWhistle-Italic';
+                src: url('/assets/fonts/ShipsWhistle-Italic.woff2 ') format('woff2');
+                font-display: swap;
+            }
+            @font-face {
+                font-family: 'ShipsWhistle-Regular';
+                src: url('/assets/fonts/ShipsWhistle-Regular.woff2 ') format('woff2');
+                font-display: swap;
+            }
+
+            strong {
+                font-family: 'ShipsWhistle-Bold', system-ui, sans-serif;
+            }
+
+            strong em {
+                font-family: 'ShipsWhistle-BoldItalic', system-ui, sans-serif;
+            }
+
+            em {
+                font-family: 'ShipsWhistle-Italic', system-ui, sans-serif;
+            }
+
+            h1, h2, h3 {
+                font-family: 'ShipsWhistle-Bold', system-ui, sans-serif;
+                font-weight: bold;
+                font-feature-settings: 'ss01';
+                text-transform: uppercase;
+            }
+
+            p {
+                font-family: 'ShipsWhistle-Regular', system-ui, sans-serif;
                 font-weight: normal;
             }
         </style>
